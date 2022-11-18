da6790d377471594092049f2d58bd0d54fb20cd8
ginji <jacques_loic@funkyduck.be>
2022-11-18 10:55:21 +0100

    Working GET ALL

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import {v4 as uuid} from 'uuid';
+
 const express = require('express');
 const sqlite = require('sqlite3').verbose();
 
@@ -12,19 +14,47 @@ db.serialize(function () {
             if (row.length === 0) {
                 var stmt = db.prepare('INSERT INTO todos VALUES(?,?,?)');
                 var obj = [{ uuid: '00000000-0000-0000-0000-000000000000', label: 'Faire mon repository', done: 0 }];
-                for(let i in obj){
-                    stmt.run(obj[i].uuid,obj[i].label,obj[i].done);
+                for (let i in obj) {
+                    stmt.run(obj[i].uuid, obj[i].label, obj[i].done);
                 }
                 stmt.finalize();
-            }else{
+            } else {
                 console.log('DB exists');
             }
         }
     });
 });
 
-const app=express();
+const app = express();
 app.use(express.json());
-app.set('port',4444);
-console.log('Server listening on port :: ',app.get('port'));
+app.set('port', 4444);
+console.log('Server listening on port :: ', app.get('port'));
 app.listen(app.get('port'));
+
+app.get('/', function (req, res) {
+    res.send('API V1');
+});
+
+// GET ALL
+app.get('/api/v1/todos', function (req, res) {
+    db.all('SELECT * FROM todos', function (err, rows) {
+        var output = [];
+        if (err) {
+            console.log(err);
+        } else {
+            if (rows.length === 0) {
+                res.send('Database is empty');
+            } else {
+                rows.forEach(function (row) {
+                    output.push({ uuid: row.uuid, label: row.label, done: row.done });
+                });
+                res.send(output);
+            }
+        }
+    });
+});
+
+// POST NEW
+app.post('/api/vi/todos',function(req,res){
+    let uuid=uuid();
+});
